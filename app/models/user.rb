47f66960af6f2854968107de52cd6f54b26a3500
Prakash Namdev <namdevprakash99@gmail.com>
2023-04-14 18:31:42 +0530

    Start App

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :invitable, :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,invite_for: 2.weeks
+end
+  
